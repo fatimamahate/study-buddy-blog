59fed70751ce6537396cf1b5c1479d497ea885a6
fatimamahate <>
2023-12-13 16:09:18 +0000

    extra setting up of admin

--- blog/admin.py
+++ blog/admin.py
@@ -5,5 +5,19 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title')}
+    list_filter = ('status', 'created_on')
+    list_display = ('title','slug', 'status','created_on')
+    search_fields = ('title', 'content')
     summernote_fields = ('content')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'body', 'post', 'created_on','approved')
+    list_filter = ('approved','created_on')
+    search_fields= ('first_name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request,queryset):
+        queryset.update(approved=True)
