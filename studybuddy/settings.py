2d6dae439c4b15dfae500e076b4930699b7060fa
fatimamahate <>
2023-12-14 16:10:06 +0000

    Add a comment section and install crispy

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -45,17 +45,19 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    # 'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'blog',
-    'django_summernote'
+    'django_summernote',
+    'crispy_forms',
 ]
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # MESSAGE_TAGS = {
 #         messages.DEBUG: 'alert-info',
